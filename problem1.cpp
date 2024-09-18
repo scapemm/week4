3859a2c07fe54b8b69dfd5ae2a079384d006902c
scapemm <a.shukurullayev@newuu.uz>
2024-09-18 12:22:40 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long a;
+    cin >> a ;
+    if(abs(a)<2147483647){
+        cout << "YES\n";
+    }
+    else cout << "NO\n";
+    return 0;
+}
